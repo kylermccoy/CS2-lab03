73db0bbaecb7968ac13944c92b17f3fac52de0da
Kyle McCoy <krm7269@rit.edu>
2019-01-30 13:50:44 -0500

    added to StackNode

--- src/rit/stu/act1/StackNode.java
+++ src/rit/stu/act1/StackNode.java
@@ -1,6 +1,7 @@
 package rit.stu.act1;
 
 import rit.cs.Stack;
+import rit.cs.Node;
 
 /**
  * A stack implementation that uses a Node to represent the structure.
@@ -9,28 +10,41 @@ import rit.cs.Stack;
  * @author Kyle McCoy   krm7269@rit.edu
  */
 public class StackNode<T> implements Stack<T> {
-
+    private int size ;
+    private Node<T> top ;
     /**
      * Create an empty stack.
      */
     public StackNode() {
+        this.size = 0 ;
+        this.top = null ;
     }
 
     @Override
     public boolean empty() {
-        return false;
+        return size == 0 ;
     }
 
     @Override
     public T pop() {
-        return null;
+        if (this.size == 0) {
+            throw new java.lang.Error("Pop on empty stack!") ;
+        }
+        T popped = this.top.getData() ;
+        this.top.setData(this.top.getNext().getData()) ;
+        this.top.setNext(this.top.getNext().getNext()) ;
+        this.size-- ;
+        return popped ;
     }
 
     @Override
-    public void push(T element) {}
+    public void push(T element) {
+        this.top = new Node<T>(element, this.top) ;
+        this.size++ ;
+    }
 
     @Override
     public T top() {
-        return null;
+        return this.top.getData() ;
     }
 }
